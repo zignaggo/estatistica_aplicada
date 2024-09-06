8793e2b4cb36082c05a2530f4c1c0d6d06c5de24
zignaggo <jvz1@aluno.ifal.edu.br>
2024-09-06 20:33:25 -0300

    feat: Coeficiente variacao

---
+++ coeficiente_variancao.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from main import sync1, asyncr1
+
+
+print("-----DATA-----")
+print("Média: ", sync1.mean())
+print(f'mediana 1: {np.median(sync1)}')
+
+print("-----DATA_2-----")
+print("Média 2: ", asyncr1.mean())
+print(f'mediana 2: {np.median(asyncr1)}')
+# bins o numero para dividir o intervalo, grpos de quanto
+# # range > minimo e máximo = diferença => multiplo do bin
+# plt.hist(sync1, 5, (65, 95))
+# plt.show()
+
+
+
+# Distribuição assimetrica a direita
+# Nesse caso a média vai ser maior que a mediana
+# O valor da média é arrastado pela cauda, nesse caso, valores a direita, maiores valores, maior média
+# plt.box(asyncr1, 5, (65, 95))
+# plt.show()
+
+
+# Distribuição assimetrica a esquerda
+# A média ficará menor que a mediana
+# O valor da média é arrastado pela cauda, nesse caso, valores a esquerda, menores valores, menor média
+
+
+# 1.5
+sns.boxplot([sync1, asyncr1])
+plt.xticks([0, 1], ['Sync', 'Async'])
+plt.show()
+
+def getBin(min, max):
+    diff = max - min 
+    print(diff)
+
--- main.py
+++ main.py
@@ -1,37 +1,32 @@
 import numpy as np
 import statistics as st
 
-data = np.array([
-    94, 84.9, 82.6, 69.5, 80.1, 79.6, 81.4, 77.8, 81.7, 78.8, 73.2, 87.9, 87.9, 93.5, 82.3, 79.3, 78.3, 71.6, 88.6, 74.6, 74.1, 80.6
-])
+sync1 = np.array([94, 84.9, 82.6, 69.5, 80.1, 79.6, 81.4, 77.8, 81.7, 78.8, 73.2, 87.9, 87.9, 93.5, 82.3, 79.3, 78.3, 71.6, 88.6, 74.6, 74.1, 80.6]) # 22
+asyncr1 = np.array([77.1, 71.7, 91, 72.2, 74.8, 85.1, 67.6, 69.9, 75.3, 71.7, 65.7, 72.6, 71.5, 78.2]) # 14
 
-data_2 = np.array(
-    [78.2, 77.1, 71.7, 91, 72.2, 74.8, 85.1, 67.6, 69.9, 75.3, 71.7, 65.7, 72.6, 71.5, 78.2]
-)
+# media = sync1.mean()
+# media_2 = asyncr1.mean()
+# print("Média: ", media)
+# print(f'mediana 1: {np.median(sync1)}')
 
-media = data.mean()
-media_2 = data_2.mean()
-print("Média: ", media)
-print(f'mediana 1: {np.median(data)}')
+# print("Média 2: ", media_2)
+# print(f'mediana 2: {np.median(asyncr1)}')
 
-print("Média 2: ", media_2)
-print(f'mediana 2: {np.median(data_2)}')
+# percentil1 = np.percentile(sync1, [ 25, 50, 75 ])
+# percentil2 = np.percentile(asyncr1, [ 25, 50, 75 ])
+# print("Quartil 1 e 3 do sync1", percentil1)
+# print("Quartil 1 e 3 do asyncr1", percentil2)
 
-percentil1 = np.percentile(data, [ 25, 50, 75 ])
-percentil2 = np.percentile(data_2, [ 25, 50, 75 ])
-print("Quartil 1 e 3 do data", percentil1)
-print("Quartil 1 e 3 do data_2", percentil2)
+# print(st.mode(sync1))
+# print(st.multimode(asyncr1))
 
-print(st.mode(data))
-print(st.multimode(data_2))
+# print('amplitude: ', sync1.ptp())
+# print('amplitude2: ', asyncr1.ptp())
 
-print('amplitude: ', data.ptp())
-print('amplitude2: ', data_2.ptp())
+# print('variancia sync1: ', sync1.var(ddof=1)) # amostra
+# print('variancia asyncr1: ', sync1.var(ddof=0))
 
-print('variancia data: ', data.var(ddof=1)) # amostra
-print('variancia data_2: ', data.var(ddof=0))
+# print('variancia2 asyncr1: ', asyncr1.var(ddof=1)) # população
+# print('variancia2 asyncr1: ', asyncr1.var(ddof=0))
 
-print('variancia2 data_2: ', data_2.var(ddof=1)) # população
-print('variancia2 data_2: ', data_2.var(ddof=0))
-
-print('desvio padrão', data.std(ddof=1))
+# print('desvio padrão', sync1.std(ddof=1))

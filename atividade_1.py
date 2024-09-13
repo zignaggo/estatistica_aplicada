f7daefef1c82a715ab4efbb608ad005ce0ab4dee
zignaggo <jvz1@aluno.ifal.edu.br>
2024-09-13 01:12:01 -0300

    feat: column stats

--- atividade_1.py
+++ atividade_1.py
@@ -1,15 +1,57 @@
+import statistics as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 # Data
-iris = pd.read_csv('./data/iris.csv')
-stock = pd.read_csv('./data/stock_data.csv')
+IRIS = pd.read_csv('./data/iris.csv')
+STOCK = pd.read_csv('./data/stock_data.csv')
 
 
+def showDictValues(data: dict):
+    for key in data:
+        print(f"{key}: {data[key]}")
 
-def getCsvStatistics(csv: pd.DataFrame, csv_name: str, ignored_columns: list):
+
+def getColumnValues(csv: pd.DataFrame, column: str):
+    if not column in csv.columns.values:
+        return None
+    return np.array(csv[column].tolist())
+
+
+def getStatistics(data):
+    mean = data.mean()
+    median = np.median(data)
+    std = data.std(ddof=1)
+    quartile = np.percentile(data, [25, 50, 75])
+    variance = data.var(ddof=1)
+    mode = st.mode(data)
+    return {
+        'max': data.max(),
+        'min': data.min(),
+        'media': mean,
+        'mediana': median,
+        'desvio_padrao': std,
+        'quartis': quartile,
+        'varianca': variance,
+        'moda': mode,
+    }
+
+
+def showColumnStats(csv: pd.DataFrame, column: str):
+    column_data = getColumnValues(csv, column)
+    if column_data is None:
+        return
+    statistics = getStatistics(column_data)
+    print(column)
+    showDictValues(statistics)
+    # box
+    sns.boxplot(column_data)
+    plt.xticks([0], [column])
+    plt.show()
+
+
+def showCsvStatistics(csv: pd.DataFrame, csv_name: str, ignored_columns: list):
     print('CSV: ', csv_name)
     print(20*'-')
     boxplot_values = []
@@ -17,25 +59,34 @@ def getCsvStatistics(csv: pd.DataFrame, csv_name: str, ignored_columns: list):
     columns_names = []
     for index, column in enumerate(csv.columns.values):
         if not column in ignored_columns:
-            column_data = np.array(csv[column].tolist())
+            column_data = csv[column].tolist()
             boxplot_values.append(column_data)
             xticks_index.append(index-1)
             columns_names.append(column)
-
+            stats = getStatistics(column_data)
             # showing values
-            print(f'Column: {column}')
-            print(f"Média: {column_data.mean():.2f}")
-            print(f"Mediana: {np.median(column_data):.2f}")
-            print(f"Desvio padrão: {column_data.std(ddof=1):.2f}")
-            print('_'*20)
-            break
+            print(column)
+            showDictValues(stats)
+            print('_'*20, '\n')
 
     sns.boxplot(boxplot_values)
-    print(xticks_index, columns_names)
     plt.xticks(xticks_index, columns_names)
     plt.show()
     print('\n')
 
 
-getCsvStatistics(iris, 'Iris', ['Id', 'Species'])
-getCsvStatistics(stock, 'Stock', ['Date',  'Name'])
+# showCsvStatistics(IRIS, 'Iris', ['Id', 'Species'])
+# showCsvStatistics(STOCK, 'Stock', ['Date',  'Name'])
+# showColumnStats(IRIS, 'SepalWidthCm')
+
+# IRIS SepalLengthCm HIST
+# column_data = getColumnValues(IRIS,  'SepalLengthCm')
+# plt.hist(column_data, 8, (4, 8))
+# plt.show()
+
+
+# STOCK High HIST
+# showColumnStats(STOCK, 'High')
+# column_data_value = getColumnValues(STOCK,  'High')
+# plt.hist(column_data_value, 5, (10, 70))
+# plt.show()
